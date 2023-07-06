72e4263393dd4581bb872e685a58ccaca3f62a98
chr96er <christianhomberg@gmail.com>
2023-07-05 21:13:51 -0500

    Add multi-version capability; finish segs_activity_daily_prototype; finish airflow dag with runtime batch request

--- dags/run-segments-daily-checkpoint.py
+++ dags/run-segments-daily-checkpoint.py
@@ -7,13 +7,16 @@ from great_expectations.data_context.types.base import (
     CheckpointConfig
 )
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 import great_expectations as gx
-context = gx.get_context(context_root_dir="../great_expectations")
+import os
+gx_context_root_dir=os.getenv('GX_CONTEXT_ROOT_DIR')
+context = gx.get_context(context_root_dir=gx_context_root_dir)
 
-from datetime import date,datetime
-
-PARTITION_DATE="2023-05-01"
-
+from datetime import date,datetime,timedelta
 
 
 with DAG(
@@ -22,16 +25,47 @@ with DAG(
     schedule_interval='@daily', catchup=False
 ) as dag:
 
-    gx_segs_activity_daily_constraints = GreatExpectationsOperator(
-        task_id="gx_segs_activity_daily_constraints",
-        data_context_root_dir="great_expectations",
-        checkpoint_name='gfw-google-827.constraints.segs_activity_daily-checkpoint',
-        checkpoint_kwargs={"validations":[
-            {
-                "batch_request": {"options": {"date": date.fromisoformat(PARTITION_DATE)}},
-                "expectation_suite_name": "gfw-google-827.constraints.segs_activity_daily"
-            }
-        ]}
-    )
+    PARTITION_DATE=str(date.today() - timedelta(days=90))
 
-    gx_segs_activity_daily_constraints
+    gx_datasource = context.get_datasource("gfw-google-827")
+    
+    for current_expectation_suite_name in [es for es in context.list_expectation_suite_names() if 'segs_activity_daily' in es and 'constraints' in es]:
+        current_expectation_suite = context.get_expectation_suite(current_expectation_suite_name)
+        current_expectation_suite_asset_name=current_expectation_suite.meta.get('asset_name')
+        gx_asset=gx_datasource.get_asset(current_expectation_suite_asset_name)
+        gx_splitter=gx_asset.splitter
+        if gx_splitter is not None:
+            DATE_PARTITION_COLUMN=gx_splitter.column_name
+            br_options={DATE_PARTITION_COLUMN: PARTITION_DATE}
+        else:
+            br_options={}
+        gx_br = gx_asset.build_batch_request(br_options)
+        gx_cp = CheckpointConfig(
+            name=f"{current_expectation_suite_name}-checkpoint",
+            validations=[
+                {
+                "batch_request": gx_br
+                }
+            ],
+            expectation_suite_name=current_expectation_suite_name
+        )
+
+        gx_validations = {
+            'validations': [
+                {
+                "batch_request": gx_br
+                }
+            ]
+        }
+
+
+        PARTITIONTIME: '2023-04-07'
+        gx_segs_activity_daily_constraints = GreatExpectationsOperator(
+            task_id=f"gx_{current_expectation_suite_name}-cp",
+            data_context_root_dir=gx_context_root_dir,
+            checkpoint_name=f"{current_expectation_suite_name}-checkpoint",
+            checkpoint_kwargs=gx_validations,
+            return_json_dict=True
+        )
+
+        gx_segs_activity_daily_constraints
