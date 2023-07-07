17fa978ede69c3d8c1026617e2da6b8126352de9
chr96er <christianhomberg@gmail.com>
2023-07-07 17:38:09 -0500

    Refactor dags; finish fragments custom expectations

---
+++ dags/run-constraints-checkpoint.py
@@ -0,0 +1,53 @@
+from airflow import DAG
+
+import great_expectations as gx
+from great_expectations_provider.operators.great_expectations import GreatExpectationsOperator
+from great_expectations.core.batch import BatchRequest
+from great_expectations.data_context.types.base import CheckpointConfig
+
+import logging
+from datetime import date,datetime,timedelta
+import os
+
+logger = logging.getLogger(__name__)
+
+gx_context_root_dir=os.getenv('GX_CONTEXT_ROOT_DIR')
+context = gx.get_context(context_root_dir=gx_context_root_dir)
+
+
+with DAG(
+    "gx_run_constraints_checkpoints", 
+    start_date=datetime(2023, 7 , 3), 
+    schedule_interval='@daily', catchup=False
+) as dag:
+
+    # TODO CHO20230705 Get date from airflow
+    PARTITION_DATE=str(date.today() - timedelta(days=90))
+
+    gx_datasource = context.get_datasource("gfw-google-827")
+    
+    # TODO CHO20230707 This is only robust as long as we have a 1:1 mapping of expectation suite to checkpoint
+    for current_expectation_suite_name in [es for es in context.list_expectation_suite_names() if 'constraints' in es]:
+        if context.get_expectation_suite(current_expectation_suite_name).expectations:
+            current_expectation_suite = context.get_expectation_suite(current_expectation_suite_name)
+            current_expectation_suite_asset_name=current_expectation_suite.meta.get('asset_name')
+            gx_asset=gx_datasource.get_asset(current_expectation_suite_asset_name)
+            gx_splitter=gx_asset.splitter
+            if gx_splitter is not None:
+                DATE_PARTITION_COLUMN=gx_splitter.column_name
+                br_options={DATE_PARTITION_COLUMN: PARTITION_DATE}
+            else:
+                br_options={}
+            gx_br = gx_asset.build_batch_request(br_options)
+
+            gx_validations = {'validations': [{"batch_request": gx_br}]}
+
+            gx_constraints = GreatExpectationsOperator(
+                task_id=f"gx_{current_expectation_suite_name}-cp",
+                data_context_root_dir=gx_context_root_dir,
+                checkpoint_name=f"{current_expectation_suite_name}-checkpoint",
+                checkpoint_kwargs=gx_validations,
+                return_json_dict=True
+            )
+
+            gx_constraints
