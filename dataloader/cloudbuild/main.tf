9fc849dcca22c774e904380bdb11136653a40498
chr96er <christianhomberg@gmail.com>
2024-05-31 00:02:49 +0200

    Increase timeout

--- dataloader/cloudbuild/main.tf
+++ dataloader/cloudbuild/main.tf
@@ -25,10 +25,12 @@ resource "google_cloudbuild_trigger" "trigger_branch" {
   }
   service_account = "projects/world-fishing-827/serviceAccounts/terraform-deployer@world-fishing-827.iam.gserviceaccount.com"
   build {
+    timeout = "3600s"
     
     step {
       id   = "docker build"
       name = "gcr.io/cloud-builders/docker"
+      timeout = "3600s"
       args = [
         "build",
         "-f",
