d911fdc110c97a5cede84ae667f349ef8c180f41
chr96er <christianhomberg@gmail.com>
2024-05-30 10:54:23 +0200

    Rename modules from alerting to dataloader

--- alerting/deploy/environments/dev/main.tf
+++ alerting/deploy/environments/dev/main.tf
@@ -2,7 +2,7 @@ provider "google" {
   project = "world-fishing-827"
 }
 
-module "anomaly_alerting_dev" {
+module "anomaly_dataloader_dev" {
   source       = "../../template"
   project      = var.project
   environment  = "dev"
