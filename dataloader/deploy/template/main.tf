0a61bd4ff65cebe1f1a0442107a247f252e74625
chr96er <christianhomberg@gmail.com>
2024-06-04 11:33:54 +0200

    Change schedule time to UTC

--- dataloader/deploy/template/main.tf
+++ dataloader/deploy/template/main.tf
@@ -149,7 +149,7 @@ resource "google_cloud_scheduler_job" "job" {
   for_each = toset(keys(yamldecode(file(var.config_path))["anomalies"]))
 
   name             = format("%s_scheduler_%s", local.project_name_dashed, each.key)
-  schedule         = "40 11 * * *"
+  schedule         = "40 9 * * *"
   attempt_deadline = "320s"
   region           = "us-central1"
   retry_config {
